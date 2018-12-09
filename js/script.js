655807a860369990e7a6ed6a40db377bda6da8f0
Manuto <emanuelefrascella08@gmail.com>
2018-12-09 12:03:48 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ function scroll_to(id) {
         scrollTop: $('#' + id).offset().top
     }, 'slow');
 }
+
+function getCreatorsAge() {
+    var date = new Date();
+    var yyyy = date.getFullYear();
+    var nascita = 2000;
+    if (date.getMonth()+1 < 8){
+        nascita += 1;
+    } else {
+        if (date.getMonth()+1 == 8 && date.getDay() < 8) {
+            nascita += 1;
+        } else {
+            nascita += 0;
+        }
+    }
+    return yyyy - nascita;
+}
